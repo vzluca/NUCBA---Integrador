0b363b1e52c3e51d3b10114b648832e7b7f25b81
vzluca <143210532+vzluca@users.noreply.github.com>
2023-11-13 16:14:25 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 window.addEventListener("scroll", function() {
     const navbar = document.getElementById("navbar");
-    if (window.scrollY > 100) { // Cambia 100 por la cantidad de desplazamiento que desees
+    if (window.scrollY > 100) { // Cambia 100 por la cantidad de desplazamiento
       navbar.classList.add("scrolled");
     } else {
       navbar.classList.remove("scrolled");
     }
-  });
+});
+
 
 
   document.addEventListener('DOMContentLoaded', function () {
@@ -15,4 +16,13 @@ window.addEventListener("scroll", function() {
     menuIcon.addEventListener('click', function () {
         navUl.classList.toggle('menu-open');
     });
+});
+
+
+// Menú hamburguesa
+    const burgerIcon = document.querySelector('.burger-icon');
+    const navbarUl = document.querySelector('.navbar nav ul');
+
+    burgerIcon.addEventListener('click', () => {
+        navbarUl.classList.toggle('show');
 });

440af9d764dd8ad08283d6112aaad6ab93dcace8
vzluca <143210532+vzluca@users.noreply.github.com>
2023-11-17 18:16:48 -0300

    Add files via upload

--- Integrador NUCBA/script.js
+++ Integrador NUCBA/script.js
@@ -1,28 +1,20 @@
-window.addEventListener("scroll", function() {
+window.addEventListener("scroll",
+  function () {
     const navbar = document.getElementById("navbar");
     if (window.scrollY > 100) { // Cambia 100 por la cantidad de desplazamiento
       navbar.classList.add("scrolled");
     } else {
       navbar.classList.remove("scrolled");
     }
-});
+  });
 
-
-
-  document.addEventListener('DOMContentLoaded', function () {
-    const menuIcon = document.querySelector('.menu-icon');
-    const navUl = document.querySelector('#navbar ul');
-
-    menuIcon.addEventListener('click', function () {
-        navUl.classList.toggle('menu-open');
-    });
-});
-
-
-// Menú hamburguesa
+document.addEventListener('DOMContentLoaded',
+  function () {
+    // Menú hamburguesa
     const burgerIcon = document.querySelector('.burger-icon');
     const navbarUl = document.querySelector('.navbar nav ul');
 
     burgerIcon.addEventListener('click', () => {
-        navbarUl.classList.toggle('show');
-});
+      navbarUl.classList.toggle('show');
+    });
+  });
